cab25c715ffabcaffb3118e9661f5d3ffe397f4e
Jericho Gian Padua <jerichopadua09@gmail.com>
2024-07-17 12:09:46 +0800

    Final Project

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,17 +1,51 @@
+import React, { useRef, useState, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 import styles from './ContactStyles.module.css';
 
 function Contact() {
+  const form = useRef();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (isSubmitted) {
+      const timer = setTimeout(() => {
+        setIsSubmitted(false);
+      }, 15000); // 15 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [isSubmitted]);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_ed6mhtn', 'template_i4llibv', form.current, {
+        publicKey: 'Co7NvoaAUKcc6jUxx',
+      })
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsSubmitted(true);
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+    form.current.reset();
+  };
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
           </label>
           <input
             type="text"
-            name="name"
+            name="user_name"
             id="name"
             placeholder="Name"
             required
@@ -23,7 +57,7 @@ function Contact() {
           </label>
           <input
             type="text"
-            name="email"
+            name="user_email"
             id="email"
             placeholder="Email"
             required
@@ -39,7 +73,8 @@ function Contact() {
             placeholder="Message"
             required></textarea>
         </div>
-        <input className="hover btn" type="submit" value="Submit" />
+        <input className="hover btn" type="submit" value="Send" />
+        {isSubmitted && <p className={styles.successMessage}>You have been sent successfully!</p>}
       </form>
     </section>
   );
