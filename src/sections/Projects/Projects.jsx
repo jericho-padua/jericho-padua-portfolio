c7ef7b11030ce6ccda862ba04d45b6b008b9049c
Jericho Gian Padua <jerichopadua09@gmail.com>
2024-07-17 16:55:22 +0800

    finished

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/bpc.ico';
-import freshBurger from '../../assets/fitlift.png';
+import fes from '../../assets/fes.png';
+import gym from '../../assets/gym.png';
+import rs from '../../assets/rs.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -9,15 +10,21 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/jericho-padua/evals/tree/master"
-          h3="BPC Faculty Evaluation System"
+          src={fes}
+          link="https://github.com/jericho-padua/bpc-faculty-evaluation-system"
+          h3="BPC Faculty Evaluation"
           p="Faculty Evaluation Website"
         />
         <ProjectCard
-          src={freshBurger}
+          src={rs}
+          link="https://github.com/jericho-padua/bpc-registration-system"
+          h3="BPC Registration System"
+          p="School Registration"
+        />
+        <ProjectCard
+          src={gym}
           link="https://github.com/jericho-padua/gym-management-system"
-          h3="Gym Management System"
+          h3="Gym Management"
           p="Fitness Website"
         />
       </div>
